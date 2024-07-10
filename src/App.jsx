cd06d59d8cf2945e0eb46033ed175a11b2e0e883
Yilikal Tesfaye Kebede <yilikalt.kebede@gmail.com>
2024-07-11 00:10:32 +0300

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import './App.css'
-
 function App() {
+  const name = 'StarGazers'
   return (
-   <h1>Hello World!</h1>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="StarGazers Group" />
+          <h1>Meet the <i style={{color: "steelblue"}}>{ name }</i></h1>
+          <p>Members of an <b>intergalactic alliance</b><br /> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={() => alert('Hi There')}>Click Me</button>
+        </hgroup>
+      </article>
+    </div>
   )
 }
 
