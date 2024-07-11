e56e7b76cf36a9abd30fbef1e408ddb4153ea8ca
Yilikal Tesfaye Kebede <yilikalt.kebede@gmail.com>
2024-07-11 17:07:33 +0300

    added useEffect at support.jsx

--- src/Components/Support.jsx
+++ src/Components/Support.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/display-name */
-import { useState } from "react";
+import { useState, useEffect} from "react";
+const pageTitle = document.title;
 
 // eslint-disable-next-line react-refresh/only-export-components
 export default  () => {
     const [count, setCount] = useState(0)
+    useEffect(() => {
+        count && (document.title = `${pageTitle} - ${count}`)
+    })
     return (
         <button className="outline" onClick={() => setCount(count + 1)}>
             {count === 0 ? "Click to Support" : `Supported ${count} Times`}  
